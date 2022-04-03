da3797b6d8d71b029a3b03aee78e59e320700027
Denisse Madeleine <dmadelei@mi-f1.msk.21-school.ru>
2022-04-03 18:11:14 +0300

    ex03

--- ex03/ticker_symbols.py
+++ ex03/ticker_symbols.py
@@ -1,31 +1,12 @@
+import sys
 if __name__ == "__main__":
-	list_of_tuples = [
-	('Russia', '25'),
-	('France', '132'),
-	('Germany', '132'),
-	('Spain', '178'),
-	('Italy', '162'),
-	('Portugal', '17'),
-	('Finland', '3'),
-	('Hungary', '2'),
-	('The Netherlands', '28'),
-	('The USA', '610'),
-	('The United Kingdom', '95'),
-	('China', '83'),
-	('Iran', '76'),
-	('Turkey', '65'),
-	('Belgium', '34'),
-	('Canada', '28'),
-	('Switzerland', '26'),
-	('Brazil', '25'),
-	('Austria', '14'),
-	('Israel', '12')]
-	new_list = dict()
-	for i in list_of_tuples:
-		if (i[1] in new_list):
-			new_list[i[1]].append(i[0])
-		else:
-			new_list[i[1]] = [i[0]]
-	for i in new_list.items():
-		for j in i[1]:
-			print(i[0], j)
+	if (len(sys.argv) != 2):
+		exit(1)
+	COMPANIES = {'Apple': 'AAPL','Microsoft': 'MSFT','Netflix': 'NFLX','Tesla': 'TESLA','Nokia': 'NOKIA'}
+	STOCKS = {'AAPL': 287.73, 'MSFT': 173.79, 'NFLX': 416.90,'TSLA': 724.88,'NOK': 3.37}
+	if sys.argv[1] not in STOCKS:
+		print('Unknown company')
+	else:
+		for i in COMPANIES.items():
+			if (sys.argv[1] == i[1]):
+				print(i[0], STOCKS[sys.argv[1]])

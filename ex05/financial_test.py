c735f279cb2774cde4997625b22cf12e5f227452
Denisse Madeleine <dmadelei@il-f5.msk.21-school.ru>
2022-04-06 15:13:54 +0300

    asd

--- ex05/financial_test.py
+++ ex05/financial_test.py
@@ -1,15 +1,18 @@
+import re
 import pytest
-import financial
+from financial import 
+
+@pytest.fixture
+def total_revenue():
+    return get_row('MSFT', 'Total Revenue')
+
+def test_invalid_row():
+    with pytest.raises(ValueError, match='row does not exist'):
+        get_row('MSFT', 'abcdef')
 
 
-def setup_module(module):
-    #init_something()
-    pass
-
-def teardown_module(module):
-    #teardown_something()
-    pass
-
-def test_upper():
-    pass
-    
+def test_return_type(total_revenue):
+    assert isinstance(total_revenue, tuple)
+	
+def test_return_len(total_revenue):
+    assert len(total_revenue) == 6
